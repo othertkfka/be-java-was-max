3da2bd44c0886dec1ef7be56087a3e48cd561330
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-05-18 16:09:04 +0900

    feat: login 메서드 구현
    로그인 정보가 Database의 userId 또는 password와 다르면 login_failed.html로 응답한다.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -16,4 +16,21 @@ public class UserController {
 
         return "redirect:/index.html";
     }
+
+    public String login(HttpRequest httpRequest, HttpResponse httpResponse) {
+        Map<String, String> requestParam = httpRequest.getRequestParam();
+        String userId = requestParam.get("userId");
+        String password = requestParam.get("password");
+
+        String viewName = "redirect:/index.html";
+        try {
+            User user = Database.findUserById(userId);
+            if (!password.equals(user.getPassword())) {
+                viewName = "/user/login_failed.html";
+            }
+        } catch (NullPointerException e) {
+            viewName = "/user/login_failed.html";
+        }
+        return viewName;
+    }
 }
