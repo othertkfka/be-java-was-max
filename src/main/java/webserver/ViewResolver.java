528525eb59000b849f4b3a71698c4f5d27db1d4e
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-05-25 14:33:11 +0900

    refactor: html 파일인 경우 String으로 한줄씩 읽을 수 있도록 변경
    html 파일인 경우 BufferedReader로 String 한 줄씩 읽어서 body에 저장하도록 변경

--- src/main/java/webserver/ViewResolver.java
+++ src/main/java/webserver/ViewResolver.java
@@ -3,7 +3,10 @@ package webserver;
 import http.HttpResponse;
 import util.RequestUtil;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class ViewResolver {
 
@@ -15,8 +18,23 @@ public class ViewResolver {
             httpResponse.setStatus(302, "FOUND");
             httpResponse.setRedirectHeader(view.replace(REDIRECT_PREFIX, ""));
         } else {
-            httpResponse.setBody(RequestUtil.findResource(view));
+            httpResponse.setBody(generateDynamicBody(view));
             httpResponse.setHeaders(view);
         }
+    }
+
+    private static byte[] generateDynamicBody(String view) throws IOException {
+        // 확장자가 html인 경우 동적 생성
+        if (view.endsWith(".html")){
+            BufferedReader br = new BufferedReader(new FileReader(RequestUtil.findResource(view)));
+            StringBuilder body = new StringBuilder();
+            String line = "";
+            while((line = br.readLine()) != null) {
+                body.append(line);
+            }
+            br.close();
+            return body.toString().getBytes();
+        }
+        return Files.readAllBytes(RequestUtil.findResource(view).toPath());
     }
 }
